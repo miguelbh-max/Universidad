ed14019543f78c6fdde2e85a566b3c66e02a5b92
miguelbh-max <miguel.bh@mazatlan.tecnm.mx>
2026-09-06 18:41:56 -0700

    Add LED blink example for Arduino Uno

--- Sist-Prog/Evidencias/01_basics/Codigo/main.ino
+++ Sist-Prog/Evidencias/01_basics/Codigo/main.ino
@@ -1,1 +1,18 @@
+/*
+ * Proyecto: Parpadeo del LED integrado (Pin 13)
+ * Placa: Arduino Uno R3
+ */
 
+const int ledPin = 13;
+
+
+void setup() {
+  pinMode(ledPin, OUTPUT);
+}
+
+void loop() {
+  digitalWrite(ledPin, HIGH); // Enciende el LED
+  delay(1000);               // Espera 1000 milisegundos (1 segundo)
+  digitalWrite(ledPin, LOW);  // Apaga el LED
+  delay(1000);               // Espera 1 segundo
+}
